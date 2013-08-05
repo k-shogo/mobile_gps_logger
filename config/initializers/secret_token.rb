a107a3f8ae975bf0cb3656ced4b12bfcbd82555b
Shogo Kawaguchi <platycod0n.ramosa@gmail.com>
2013-08-05 23:15:53 +0900

    initialize rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MobileGpsLogger::Application.config.secret_key_base = 'be8ee0582c4384d06b60557c4303defbb664fe47633506b702607f5b023c66264df0f19cfe039a2dcd33ab212edc27502f930548bc96a61bb16781cf2ee55667'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MobileGpsLogger::Application.config.session_store :cookie_store, key: '_mobile_gps_logger_session'
