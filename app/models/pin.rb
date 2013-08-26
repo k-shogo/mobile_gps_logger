76d32a50fe07a6f3753298ca432b5c3263173ca8
Shogo Kawaguchi <platycod0n.ramosa@gmail.com>
2013-08-26 17:46:50 +0900

    static map urlの生成にキーワード付き引数を利用できるように変更
    zoom及びsizeをキーワード引数で指定できるように変更

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,8 +2,11 @@ class Pin < ActiveRecord::Base
   reverse_geocoded_by :latitude, :longitude
   # after_validation :reverse_geocode
 
-  def static_map_url
-
-    "http://maps.google.com/maps/api/staticmap?size=600x400&sensor=false&zoom=16&markers=#{self.latitude}%2C#{self.longitude}"
+  def static_map_url zoom: 16, size: '600x400'
+    "http://maps.google.com/maps/api/staticmap" +
+      "?size=" + size +
+      "&sensor=false" +
+      "&zoom=" + zoom.to_s +
+      "&markers=#{self.latitude}%2C#{self.longitude}"
   end
 end
